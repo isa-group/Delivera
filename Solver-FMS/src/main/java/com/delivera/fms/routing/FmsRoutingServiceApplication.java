bec3549f852c04c239dd2b31f5a08bd77e640cc1
ThePayan <pedropayanaguilera@gmail.com>
2026-06-16 12:20:38 +0200

    feat: Inicializar proyecto Spring-Boot

---
+++ Solver-FMS/src/main/java/com/delivera/fms/routing/FmsRoutingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.delivera.fms.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsRoutingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmsRoutingServiceApplication.class, args);
+    }
+}
