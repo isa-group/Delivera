73d8e352349e2ed22c94432dfbbf37aa70a5118a
davposmar <davposmar@alum.us.es>
2026-06-26 12:51:04 +0200

    feat: add login context endpoint

---
+++ backend/src/main/java/com/delivera/auth/dto/DeliveraOrgContext.java
@@ -0,0 +1,21 @@
+package com.delivera.auth.dto;
+
+import java.util.UUID;
+
+import com.delivera.model.WorkerRole;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class DeliveraOrgContext {
+        UUID companyId;
+        WorkerRole role;
+        String companyName;
+        String orgHandle;
+        String orgName;
+}
+        
---
+++ backend/src/main/java/com/delivera/auth/repository/AuthRepository.java
@@ -0,0 +1,41 @@
+package com.delivera.auth.repository;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.delivera.auth.dto.DeliveraOrgContext;
+import com.delivera.model.Worker;
+
+
+public interface AuthRepository  extends JpaRepository<Worker, UUID> {
+
+    
+@Query("""
+    SELECT new com.delivera.auth.dto.DeliveraOrgContext(
+        c.id,
+        w.role,
+        c.name,
+        o.handle,
+        o.name
+    )
+    FROM Worker w
+    JOIN w.company c
+    JOIN c.organization o
+    JOIN w.user u
+    WHERE u.id = :userId
+    ORDER BY w.createdAt ASC
+    """)
+    List<DeliveraOrgContext> findOrgContextByUserId(
+        @Param("userId") UUID userId,
+        Pageable pageable
+    );
+    
+
+}
