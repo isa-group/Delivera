85d8016e836f2d843cf8617704b395ee701a6649
davposmar <davposmar@alum.us.es>
2026-07-09 14:50:34 +0200

    feat: authRateLimit

--- auth-service/src/main/java/com/delivera/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/delivera/auth/controller/AuthController.java
@@ -279,7 +279,8 @@ public class AuthController {
         String ip = getIp(httpRequest);
         String deviceId = getDeviceId(httpRequest);
         String userAgent = getUserAgent(httpRequest);
-        
+        authRateLimiter.check("REFRESH:" + ip);
+
         RefreshToken refreshToken = refreshTokenService.validateAndGet(token);
         Credential credential = refreshToken.getCredential();
         DeliveraOrgContext orgInfo;
@@ -333,6 +334,8 @@ public class AuthController {
     ){
 
         String token = getRefreshTokenFromCookies(httpRequest);
+        String ip = getIp(httpRequest);
+        authRateLimiter.check("DEVICE:" + ip);
         RefreshToken refreshToken = refreshTokenService.validateAndGet(token);
 
         return ResponseEntity.ok().body(
@@ -354,7 +357,7 @@ public class AuthController {
         String ip = getIp(httpRequest);
         String deviceId = getDeviceId(httpRequest);
         String userAgent = getUserAgent(httpRequest);
-
+        authRateLimiter.check("DEVICE:" + ip);
         RefreshToken refreshToken = refreshTokenService.validateAndGet(token);
         
         authService.avoidAttacksWithCorrectIdentifier(
@@ -380,7 +383,8 @@ public class AuthController {
         String ip = getIp(httpRequest);
         String deviceId = getDeviceId(httpRequest);
         String userAgent = getUserAgent(httpRequest);
-
+        authRateLimiter.check("DEVICE:" + ip);
+        
         RefreshToken refreshToken = refreshTokenService.validateAndGet(token);
         
         authService.avoidAttacksWithCorrectIdentifier(
